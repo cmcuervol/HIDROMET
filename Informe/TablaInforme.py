f45ae4712897acbcf3710c2c61a767b75b9c1268
cmcuervol <cmcuervol@unal.edu.co>
2019-01-27 21:23:47 -0500

    Week January 21th to 27th

--- Informe/TablaInforme.py
+++ Informe/TablaInforme.py
@@ -87,10 +87,21 @@ JuanMarica.setFont("Avenir", 24)
 #         [u'Copacabana',   u'Río Medellín'                         ,'2019-01-08'  , '22:09'     ],
 #         [u'Copacabana',   u'Río Medellín'                         ,'2019-01-08'  , '23:20'     ],
 #        ]
-# week  January 14th to 20th
+# # week  January 14th to 20th
+# data = [['Municipio',     'Zona alertada'                                       ,'Fecha alerta','Hora Alerta'],
+#         [u'Medellín',     u'Columna de humo en San Javier'                      ,'2019-01-18'  , '15:50'     ],
+#         [u'Medellín',     u'Columnas de humo en San Cristobal'                  ,'2019-01-19'  , '10:55'     ],
+#        ]
+# week  January 21th to 27th
 data = [['Municipio',     'Zona alertada'                                       ,'Fecha alerta','Hora Alerta'],
-        [u'Medellín',     u'Columna de humo en San Javier'                      ,'2019-01-18'  , '15:50'     ],
-        [u'Medellín',     u'Columnas de humo en San Cristobal'                  ,'2019-01-19'  , '10:55'     ],
+        [u'Medellín',     u'Columna de humo en San Javier (El Salado)'          ,'2019-01-21'  , '16:30'     ],
+        [u'Medellín',     u'Columna de humo en San Javier (El Salado)'          ,'2019-01-22'  , '10:48'     ],
+        [u'Medellín',     u'Columna de humo en San Javier (El Socorro)'         ,'2019-01-24'  , '10:04'     ],
+        [u'Medellín',     u'Columna de humo en San Javier (El Socorro)'         ,'2019-01-25'  , '11:45'     ],
+        [u'Medellín',     u'Columna de humo en San Blas'                        ,'2019-01-23'  , '11:30'     ],
+        [u'Medellín',     u'Columna de humo en San Cristobal'                   ,'2019-01-23'  , '13:48'     ],
+        [u'Medellín',     u'Columna de humo en Bello Oriente'                   ,'2019-01-23'  , '18:00'     ],
+        [u'Bello',        u'Columna de humo en el cerro Quitasol'               ,'2019-01-26'  , '16:15'     ],
        ]
 
 # More width to zone column
@@ -101,7 +112,7 @@ t=Table(data,[2.0*inch,6.5*inch,1.7*inch,1.5*inch], len(data)*[0.4*inch])
 t.setStyle(TableStyle([('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),\
                        ('BOX', (0,0), (-1,-1), 0.25, colors.black),\
                        ('ALIGN',(0,0),(-1,-1),'CENTER'),\
-                       ('ALIGN',(1,0),(1,-1),'LEFT'),\
+                       ('ALIGN',(1,1),(1,-1),'LEFT'),\
                        # ('VALIGN',(0,0),(-1,-1),'MIDDLE'),\
                        ('VALIGN',(0,0),(-1,-1),'TOP'),\
                        ('VALIGN',(0,0),(-1,0),'TOP'),\
@@ -110,14 +121,16 @@ t.setStyle(TableStyle([('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),\
                        ('FONTSIZE', (0, 1), (-1, -1), 18),\
                        ('TEXTCOLOR',(0, 0),(-1,0),colors.white),\
                        # # # Municipio
-                       ('SPAN',   (0,1), (0,2)),\
-                       ('VALIGN', (0,1), (0,2),'MIDDLE'),\
+                       ('SPAN',   (0,1), (0,7)),\
+                       ('VALIGN', (0,1), (0,7),'MIDDLE'),\
                        # # # zona
-                       # ('SPAN',   (1,2), (1,5)),\
-                       # ('VALIGN', (1,2), (1,5),'MIDDLE'),\
+                       ('SPAN',   (1,1), (1,2)),\
+                       ('VALIGN', (1,1), (1,2),'MIDDLE'),\
+                       ('SPAN',   (1,3), (1,4)),\
+                       ('VALIGN', (1,3), (1,4),'MIDDLE'),\
                        # # # # fecha
-                       # ('SPAN',   (2,1), (2,5)),\
-                       # ('VALIGN', (2,1), (2,5),'MIDDLE'),\
+                       ('SPAN',   (2,5), (2,7)),\
+                       ('VALIGN', (2,5), (2,7),'MIDDLE'),\
                        # # # hora
                        # ('SPAN',   (3,1), (3,2)),\
                        # ('VALIGN', (3,1), (3,2),'MIDDLE'),\
